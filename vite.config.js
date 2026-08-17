0b416602cd52298b9ed09e9614a2ec23e70e71e1
wedding-core <mkpos190498@gmail.com>
2026-08-18 01:16:35 +0800

    Update favicon ke logo.svg

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my-wedding-invite/', // Tambahkan baris ini sesuai nama repository GitHub Anda
 })
